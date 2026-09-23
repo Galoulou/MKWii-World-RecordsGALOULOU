175f045a4bcd3dfba91967158d37c4beadb7c220
Galoulou <twitchgaloulou@gmail.com>
2026-09-23 20:12:47 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://dkzqdtrrktlegsqvlsqy.supabase.co";
-const SUPABASE_KEY = "sb_publishable_I8ohZeiDmvfzt2JZQwocVQ_eokS0jTG";
+const SUPABASE_URL = "https://dkzqdtrrktlegsqvlsgy.supabase.co";
+const SUPABASE_KEY = "TA_CLÉ_PUBLISHABLE";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
