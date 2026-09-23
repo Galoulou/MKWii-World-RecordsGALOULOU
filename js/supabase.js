319a2bbc7b43fc7d489d212b7410fc24a725150e
Galoulou <twitchgaloulou@gmail.com>
2026-09-23 19:55:52 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dkzqdtrrktlegsqvlsqy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_I8ohZeiDmvfzt2JZQwocVQ_eokS0jTG";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
